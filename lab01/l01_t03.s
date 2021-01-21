57966ba55005c9f9a80246c6a134f27b7094d05c
Nausher Rao <SherRao15@gmail.com>
2021-01-21 14:39:00 -0500

    Update l01_t03.s

--- lab01/l01_t03.s
+++ lab01/l01_t03.s
@@ -19,7 +19,7 @@ MOV R1, #14     // Store decimal 14 in register R1
 ADD R2, R1, R0  // Add the contents of R0 and R1 and put result in R2
 MOV R3, #8 //Store decimal value 8 into register R3
 ADD R3, R3, R3 //Add the value in R3 to itself and store it in itself too 
-ADD #4, R3, R4 //Tried to add the immediate decimal value of 4 to the value in regsiter R3 and stores in R4
+ADD R3, R4, #4 //Tried to add the immediate decimal value of 4 to the value in regsiter R3 and stores in R4
 
 // End program
 _stop:
