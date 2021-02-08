ea1e5dbf91e6d3c81fac7942866531d498541ae7
Nausher Rao <SherRao15@gmail.com>
2021-02-08 00:20:56 -0500

    Finished Lab 3

--- Lab 3/l03_t03.s
+++ Lab 3/l03_t03.s
@@ -18,11 +18,18 @@ Date:    2021-02-01
 .global _start
 _start:
 
+
 LDR R0, =LEDS		// LEDs base address
 LDR R1, =TIMER		// private timer base address
+
 LDR R2, =LED_BITS	// value to set LEDs
-LDR R3, =DELAY_TIME	// timeout = 1/(200 MHz) x 200x10^6 = 1 sec
-STR R3, [R1]		// write timeout to timer load register
+LDR R3, [R4]
+
+LDR R5, =COUNTER	// timeout = 1/(200 MHz) x 200x10^6 = 1 sec
+LDR R3, [R5]
+
+
+STR R3, [R1]		// write timeout to timer load 
 MOV R3, #0b011		// set bits: mode = 1 (auto), enable = 1
 STR R3, [R1, #0x8]	// write to timer control register
 LOOP:
@@ -36,6 +43,11 @@ ROR	R2, #1			// rotate the LED bits
 B LOOP
 
 .data
-LED_BITS: .word 0x0F0F0F0F
-DELAY_TIME: .word 200000000
+LED_BITS:
+.word 0x0F0F0F0F
+COUNTER:
+.word 200000000
+
 .end
+
+// Changing LED_BITS and COUNTER to be stored in memory doesnt change the output.
